0a9d09cfd35084ee6085d802926a9fe4d4ffe9a9
Alisya Rahma Safitra <117332257+alisyasaf@users.noreply.github.com>
2023-02-08 23:30:22 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1>Selamat Datang,</h1>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="img/bunny 1.jpg" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Hype Boy</h5>
+                <p>lalalallalalalal</p>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/bunny 2.jpg" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Ditto</h5>
+                <p>Stay in the middle</p>
+              </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/bunny 3.jpg" alt="Third slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>NewJeans</h5>
+                <p>...</p>
+              </div>
+        </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <meta name="theme-color" content="#000">
+    
+    <meta name="description" content="Description"/>
+    <meta name="robots" content="index,follow">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>MITRA TASPEN</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light shadow p-3 mb-5 bg-white rounded">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="img/logo.png" alt="logo" width="105" height="45">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse">
+            <div>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/login">Login</a>
+              </li>
+            </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+    <div class="container mt-4">
+        @yield('container')
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+
+    <!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-dark fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>KCU SEMARANG
+            </h6>
+            <p>
+                Jl. Mataram No.892-894, Wonodri, Kec. Semarang Sel., Kota Semarang, Jawa Tengah 50242
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Hubungi Kami
+            </h6>
+            <p>
+              (024) 8314225
+            </p>
+            <p>
+              taspen@taspen.co.id
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Jam Operasional
+            </h6>
+            <p>
+              Senin-Kamis : 08.00 - 17.00 WIB
+            </p>
+            <p>
+              Jumat : 07.30 - 16.30 WIB
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <a class="logo" href="#">
+                <img src="img/logo.png" alt="logo" width="120" height="55">
+            </a>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2023 Copyright:
+      <a class="text-reset fw-bold" href="https://www.taspen.co.id/">TASPEN</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+  </body>
+</html>
