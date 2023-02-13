49fe2c341b53ec73c137ae85b433f26cf012af3c
ndeeen <ratihadiningrum@gmail.com>
2023-02-13 14:58:28 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,9 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 </head>
 
 <body class="d-flex justify-content-center" style="background-color:#061755">
@@ -17,12 +20,12 @@
                 <form>
                     <div class="form-group d-flex justify-content-center">
                         <i class="bi bi-person" style="color: white; font-size: 30px"></i>
-                        <input class="form-control" type="username" id="username" placeholder="Username">
+                        <input class="form-control text-light" type="username" id="username" placeholder="Username" style="border-radius: 100px; background-color:white; border-color:white;">
                     </div><br>
 
                     <div class="form-group d-flex justify-content-center">
                         <i class="bi-lock-fill" style="color: white; font-size: 28px"></i>
-                        <input class="form-control" type="password" id="password" placeholder="Password">
+                        <input class="form-control text-white" type="password" id="password" placeholder="Password" style="border-radius: 100px; background-color:white; border-color:white">
                     </div><br>
 
                     <div class="form-group d-flex justify-content-center">
@@ -31,7 +34,7 @@
                     </div><br>
                     
                     <div class="d-flex justify-content-center">
-                        <input class="btn btn-light" type="submit" name="" id="SIGN IN">
+                        <button class="btn btn-light" type="button" name="" id="SIGN IN" style="border-radius: 100px; color: #061755; width: 150px">Masuk</button>
                     </div>
                 </form>
             </div>
